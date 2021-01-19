5a3be10818b840a2fff3a7ecc22c25821f2ec10b
ZXC <zhxxch@outlook.com>
2021-01-19 14:16:29 +0800

    comments, footnotes, endnotes

--- docx2txt.cs
+++ docx2txt.cs
@@ -13,19 +13,28 @@ namespace Docx2txt
 {
     class Program
     {
-        static void Main(string[] args)
-        {
+		static void PrintText(ZipArchive DocZip, string entry_name){
 			XmlNamespaceManager WpNs = new XmlNamespaceManager(new NameTable());
         	WpNs.AddNamespace("w",
 				"http://schemas.openxmlformats.org/wordprocessingml/2006/main");
-			FileStream DocFile = File.Open(args[0],FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
-			ZipArchive DocZip = new ZipArchive(DocFile, ZipArchiveMode.Read);
-			Stream WpXml = DocZip.GetEntry(
-			"word/document.xml").Open();
-			IEnumerable<XElement> ParagraphElems = XDocument.Load(WpXml).XPathSelectElements("//w:p", WpNs);
+			ZipArchiveEntry wpxml_part = DocZip.GetEntry(entry_name);
+			if(wpxml_part==null)return;
+			Stream xml_stream = wpxml_part.Open();
+			IEnumerable<XElement> ParagraphElems = XDocument.Load(xml_stream).XPathSelectElements("//w:p", WpNs);
 			foreach(XElement pnode in ParagraphElems){
 				Console.WriteLine(pnode.Value);
 			}
+			xml_stream.Dispose();
+		}
+        static void Main(string[] args)
+        {
+			
+			FileStream DocFile = File.Open(args[0],FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+			ZipArchive DocZip = new ZipArchive(DocFile, ZipArchiveMode.Read);
+			PrintText(DocZip, "word/comments.xml");
+			PrintText(DocZip, "word/document.xml");
+			PrintText(DocZip, "word/footnotes.xml");
+			PrintText(DocZip, "word/endnotes.xml");
 			DocZip.Dispose();
 			DocFile.Dispose();
         }
